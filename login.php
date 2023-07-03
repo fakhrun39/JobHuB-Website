e35a6ea0f625283278a502c02fc87e6892a51875
fakhrun39 <48872739+fakhrun39@users.noreply.github.com>
2023-07-03 16:31:55 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,120 @@
 <?php
-// Koneksi ke database
-$host = 'nama_host_database';
-$username = 'nama_pengguna';
-$password = 'kata_sandi';
-$database = 'nama_database';
+session_start();
+$host = 'localhost';
+$username = 'root';
+$password = 'fakhrun';
+$database = 'db_jobhub';
 
 $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    die('Koneksi gagal: ' . mysqli_connect_error());
+}
+$err = "";
+$username = "";
+$ingatpassword = "";
+if(isset($_COOKIE['cookie_username'])){
+    $cookie_username = $_COOKIE['cookie_username'];
+    $cookie_password = $_COOKIE['cookie_password'];
+
+    $sql1 = "select * from login where username = '$cookie_username'";
+    $q1   = mysqli_query($conn,$sql1);
+    $r1   = mysqli_fetch_array($q1);
+    if($r1['password'] == $cookie_password){
+        $_SESSION['session_username'] = $cookie_username;
+        $_SESSION['session_password'] = $cookie_password;
+    }
 }
 
-// Mendapatkan data dari formulir login
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-// Melakukan validasi login
-$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-$result = mysqli_query($conn, $query);
-
-if (mysqli_num_rows($result) == 1) {
-    // Login berhasil
-    echo "Selamat datang, " . $username . "!";
-} else {
-    // Login gagal
-    echo "Username atau password salah.";
+if(isset($_SESSION['session_username'])){
+    header("location:dashboard.php");
+    exit();
 }
 
-// Menutup koneksi database
-mysqli_close($conn);
+if(isset($_POST['login'])){
+    $username   = $_POST['username'];
+    $password   = $_POST['password'];
+    $ingatpassword   = $_POST['ingatpassword'];
+
+    if($username == '' or $password == ''){
+        $err .= "<li>Silakan masukkan username dan juga password.</li>";
+    }else{
+        $sql1 = "select * from users where username = '$username'";
+        $q1   = mysqli_query($conn,$sql1);
+        $r1   = mysqli_fetch_array($q1);
+
+        if($r1['username'] == ''){
+            $err .= "<li>Username <b>$username</b> tidak tersedia.</li>";
+        }elseif($r1['password'] != $password){
+            $err .= "<li>Password yang dimasukkan tidak sesuai.</li>";
+        }       
+        
+        if(empty($err)){
+            $_SESSION['session_username'] = $username; //server
+            $_SESSION['session_password'] = $password;
+
+            if($ingatpassword == 1){
+                $cookie_name = "cookie_username";
+                $cookie_value = $username;
+                $cookie_time = time() + (60 * 60 * 24 * 30);
+                setcookie($cookie_name,$cookie_value,$cookie_time,"/");
+
+                $cookie_name = "cookie_password";
+                $cookie_value = $password;
+                $cookie_time = time() + (60 * 60 * 24 * 30);
+                setcookie($cookie_name,$cookie_value,$cookie_time,"/");
+            }
+            header("location:dashboard.php");
+        }
+    }
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+</head>
+<body>
+<div class="container my-4">    
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+        <div class="panel panel-info" >
+            <div class="panel-heading">
+                <div class="panel-title">Login dan Masuk Ke Sistem</div>
+            </div>      
+            <div style="padding-top:30px" class="panel-body" >
+                <?php if($err){ ?>
+                    <div id="login-alert" class="alert alert-danger col-sm-12">
+                        <ul><?php echo $err ?></ul>
+                    </div>
+                <?php } ?>                
+                <form id="loginform" class="form-horizontal" action="" method="post" role="form">       
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="login-username" type="text" class="form-control" name="username" value="<?php echo $username ?>" placeholder="username">                                        
+                    </div>
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                    </div>
+                    <div class="input-group">
+                        <div class="checkbox">
+                        <label>
+                            <input id="login-remember" type="checkbox" name="ingatpassword" value="1" <?php if($password == '1') echo "checked"?>> Ingat Aku
+                        </label>
+                        </div>
+                    </div>
+                    <div style="margin-top:10px" class="form-group">
+                        <div class="col-sm-12 controls">
+                            <input type="submit" name="login" class="btn btn-success" value="Login"/>
+                        </div>
+                    </div>
+                </form>    
+            </div>                     
+        </div>  
+    </div>
+</div>
+</body>
+</html>
