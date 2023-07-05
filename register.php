bff2ee6a93ff1e514b65e2e99632381f61f75e84
fakhrun39 <48872739+fakhrun39@users.noreply.github.com>
2023-07-05 14:23:28 +0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,35 +1,28 @@
 <?php
 // Koneksi ke database
-$host = 'nama_host_database';
-$username = 'nama_pengguna';
-$password = 'kata_sandi';
-$database = 'nama_database';
-
-$conn = mysqli_connect($host, $username, $password, $database);
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-
+include 'koneksi.php';
 // Mendapatkan data dari formulir register
 $username = $_POST['username'];
-$password = $_POST['password'];
 $email = $_POST['email'];
+$password = $_POST['password'];
+
 // Memeriksa apakah username sudah digunakan
 $query = "SELECT * FROM users WHERE username = '$username'";
 $result = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($result) > 0) {
     // Username sudah digunakan
-    echo "Username sudah terdaftar. Silakan gunakan username lain.";
+    echo 'Username sudah terdaftar. Silakan gunakan username lain.';
 } else {
     // Menambahkan pengguna baru ke tabel users
-    $query = "INSERT INTO users (username,email, password) VALUES ('$username','$email' '$password')";
+    $query = "INSERT INTO users (username, email, password) VALUES ('$username','$email', '$password')";
     if (mysqli_query($conn, $query)) {
         // Registrasi berhasil
-         echo '<script>alert("Registrasi berhasil. Silakan login dengan akun baru."); window.location.href = "dashboard.html";</script>'; // Redirect ke dashboard.html setelah menampilkan notifikasi berhasil
+        echo 'Registrasi berhasil. Silakan login dengan akun baru.';
+        echo '<script>window.location.href = "index.html";</script>';
     } else {
         // Registrasi gagal
-        echo '<script>alert("Registrasi gagal. Silakan coba lagi."); window.location.href = "register.html";</script>'; // Redirect kembali ke halaman register.html setelah menampilkan notifikasi gagal
+        echo 'Registrasi gagal. Silakan coba lagi.';
     }
 }
 
